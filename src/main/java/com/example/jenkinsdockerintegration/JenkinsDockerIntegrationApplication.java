0755483178d12e96fd43784bb8f62ff00ca0026e
Ravi kumar gupta <0011223344ravi@gmail.com>
2022-10-11 12:28:07 +0530

    first commit

---
+++ src/main/java/com/example/jenkinsdockerintegration/JenkinsDockerIntegrationApplication.java
@@ -0,0 +1,15 @@
+package com.example.jenkinsdockerintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerIntegrationApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Baba");
+        SpringApplication.run(JenkinsDockerIntegrationApplication.class, args);
+    }
+
+}
